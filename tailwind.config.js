4f9b0f7dc46010c384cb29634b58a179a9981209
melvis8 <melvisnajela@gmail.com>
2024-04-20 16:28:40 +0100

    v2

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        backgroundImage: {
+         'message' : "url(./img/message.png)",
+         'key' : "url(./img/key.png)", 
+        colors: {
+          clifford: '#da373d',
+
+        }
+      }
+    }
+  }
+  }
